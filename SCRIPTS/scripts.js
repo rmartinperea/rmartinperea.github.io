539e1ee2772f4fb167549a5cee1f8010537facbe
rmartinperea <rmartinp.it@gmail.com>
2025-01-02 14:27:50 +0100

    .

--- SCRIPTS/scripts.js
+++ SCRIPTS/scripts.js
@@ -9,19 +9,23 @@ var contactBar = document.querySelector('.header-contacto');
 
 // Función que se ejecuta al hacer scroll
 function toggleHeader() {
-    // Si el desplazamiento es mayor que la altura de la barra de contacto
-    if (window.pageYOffset > contactBar.offsetHeight) {
-        // Ocultamos la barra de contacto
-        contactBar.style.top = '-60px';  // Ajusta este valor según la altura de tu barra
-        // Movemos el header más hacia arriba al hacer scroll
-        header.style.top = '-30px';  // Ajusta este valor según cuánto quieres que suba
-    } else {
-        // Cuando volvemos hacia arriba, mostramos la barra de contacto
-        contactBar.style.top = '0';  // Vuelve a la posición original
-        // Restauramos el padding original y el top del header
-        header.style.top = '0';  // El header vuelve a su posición original
-    }
+  // Si el desplazamiento es mayor que la altura de la barra de contacto
+  if (window.pageYOffset > contactBar.offsetHeight) {
+      // Ocultamos la barra de contacto dinámicamente
+      contactBar.style.top = `-${contactBar.offsetHeight}px`; 
+
+      // Movemos el header más hacia arriba de forma proporcional
+      header.style.top = `-${header.offsetHeight / 2}px`; 
+  } else {
+      // Cuando volvemos hacia arriba, mostramos la barra de contacto
+      contactBar.style.top = '0';  // Vuelve a la posición original
+
+      // Restauramos la posición original del header
+      header.style.top = '0';  
+  }
 }
+
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
